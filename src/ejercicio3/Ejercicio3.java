4975f77e616c2efd3b59fc03d7d70ff8a9f21ad1
Luciano <mairiciosoto@gmail.com>
2022-12-07 21:40:09 -0300

    version final

---
+++ src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,30 @@
+package ejercicio3;
+
+public class Ejercicio3 {
+	public static void main(String[] args) throws Exception {
+
+		int resultado = suma(10, 20);
+
+		System.out.println(resultado);
+
+		Coche miCoche = new Coche();
+		miCoche.AumentarPuertas();
+
+		System.out.println(miCoche.cantidadPuertas);
+	}
+
+	public static int suma(int numero1, int numero2) {
+		int resultado = numero1 + numero2;
+
+		return resultado;
+	}
+}
+
+class Coche {
+
+	public int cantidadPuertas;
+
+	public void AumentarPuertas() {
+		this.cantidadPuertas++;
+	}
+}
